027ce2c8863254966a4776ff20cb0450e8580956
Kacy Trujillo <kclnto@gmail.com>
2020-08-31 19:15:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,9 @@ def turn_count
   board = [" "," "," "," "," "," "," "," "," "]
   count = 0
   board.each |turn|
-    count += 1 
+    if turn == "X" || turn == "O"
+      count += 1
+    end
     puts count
   end
 end
